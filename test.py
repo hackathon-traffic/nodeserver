5baf91fe3359e712e87ef10a5955497f848ecf75
Willjay <willjay90.t@gmail.com>
2019-02-09 11:09:35 -0800

    ffmpeg script

--- test.py
+++ test.py
@@ -1,6 +1,18 @@
-import argparse
+import subprocess
+import datetime
+import sched, time
 
-ap = argparse.ArgumentParser()
-ap.add_argument('-i','--image', required=True, help='')
-args = ap.parse_args()
-print(args.image)
+
+s = sched.scheduler(time.time, time.sleep)
+def getImage(sc): 
+    timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    script = 'ffmpeg -y -i rtmp://wzmedia.dot.ca.gov:1935/D4/E4_at_Port_Chicago_Hwy.stream -vframes 1 ' + 'input/' + timestamp + '.jpg'
+
+    subprocess.call(script, shell=True)
+    s.enter(1, 1, getImage, (sc,))
+
+s.enter(1, 1, getImage, (s,))
+s.run()
+
+
+
