ae46de778def4be217d144eddbad354d5dede33b
williamleebitbucket <william.lee.bitbucket@gmail.com>
2019-02-08 22:40:03 -0800

    + Testing web scraping

--- controller.js
+++ controller.js
@@ -55,6 +55,46 @@ router.post("/repos(/*)?", (req, res) => {
 });
 
 
+
+//Read from mpeg server
+router.get("/getImage", (req, res) => {
+
+
+    const puppeteer = require('puppeteer');
+    const url = 'https://www.reddit.com';
+
+    puppeteer
+    .launch()
+    .then(function(browser) {
+        return browser.newPage();
+    })
+    .then(function(page) {
+        return page.goto(url).then(function() {
+            return page.content();
+        });
+    })
+    .then(function(html) {
+        return 
+        console.log(html);
+    })
+    .catch(function(err) {
+        //handle error
+    });
+    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+    var request = new XMLHttpRequest();
+    request.open("GET", "http://www.dot.ca.gov/d4/d4cameras/ct-cam-pop-N1_at_Presidio_Tunnel.html", true);
+    request.send(null);
+    request.onreadystatechange = function() {
+      if (request.readyState == 4)
+         console.log(request);
+
+
+    };
+    console.log('We are sending back a response');
+    res.send('Hello World'); 
+});
+ 
+
 function readFromUrl(url) {
 
     request({ url, encoding: null }, (err, resp, buffer) => {
